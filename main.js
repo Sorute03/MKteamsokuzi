f7353f791c43fe189c2bb0fe28b95f063184e53c
レモちゃん♪ <151602158+Sorute03@users.noreply.github.com>
2026-06-07 02:44:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2062,7 +2062,7 @@ function sendHistoryToGAS() {
   alert("共有保存（GAS）はまだ実装されていません");
 }
 
-const OVERLAY_GAS_URL = "https://script.google.com/macros/s/AKfycbw9mZJGnCFMgNlBaY8AA8BNKrmxDBeKDHyI622GKpaJn1DEKvBEON4wexP1Q2nM6y_I/exec"; // ← あなたの GAS URL
+const OVERLAY_GAS_URL = "https://script.google.com/macros/s/AKfycbxtV9LiJJqIaxDU8V6dxW0BvgDH7Y6lyrak7pkvsC3fXKRT6bV4Aj9ppZ4jYWGxwAMf/exec"; // ← あなたの GAS URL
 
 // ▼ 送信用チャンネルを作成
 const bc = new BroadcastChannel("mk_overlay");
@@ -2087,7 +2087,7 @@ function openOverlayWindow() {
 }
 
 async function sendOverlay(payload) {
-  await fetch("https://script.google.com/macros/s/AKfycbw9mZJGnCFMgNlBaY8AA8BNKrmxDBeKDHyI622GKpaJn1DEKvBEON4wexP1Q2nM6y_I/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbxtV9LiJJqIaxDU8V6dxW0BvgDH7Y6lyrak7pkvsC3fXKRT6bV4Aj9ppZ4jYWGxwAMf/exec", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
